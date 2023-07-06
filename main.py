067c2e5ffb2c20071b5308a8ddb080a890630386
Rakotonimanana T. Andritiana Steve <pro@andritiana.tech>
2023-07-06 11:32:20 +0300

    fix

--- main.py
+++ main.py
@@ -11,40 +11,40 @@ import io
 
 app = FastAPI()
 
-# Endpoint pour ajouter une personne
-@app.post("/add")
-async def add_person(images: List[UploadFile] = File(...), data: UploadFile = File(...)):
-    # Créer un dossier unique pour chaque personne
-    person_folder = str(uuid.uuid4())
-    person_path = Path("persons") / person_folder
-    person_path.mkdir(parents=True, exist_ok=True)
+@app.post("/search")
+async def search_person(image: UploadFile = File(...)):
+    # Charger l'image et détecter les visages
+    image_data = image.file.read()
+    image = Image.open(io.BytesIO(image_data))
+    image = np.array(image)  # Convertir l'image en tableau NumPy
+    face_locations = face_recognition.face_locations(image)
 
-    # Enregistrer les images dans le dossier de la personne
-    image_paths = []
-    for image in images:
-        image_path = person_path / image.filename
-        image_paths.append(str(image_path))
-        with open(image_path, "wb") as f:
-            f.write(image.file.read())
+    if len(face_locations) == 0:
+        return {"message": "No face detected"}
 
-    # Enregistrer le fichier JSON de données dans le dossier de la personne
-    data_path = person_path / data.filename
-    with open(data_path, "wb") as f:
-        f.write(data.file.read())
+    # Trouver le visage le plus grand
+    largest_face_location = max(face_locations, key=lambda loc: (loc[2] - loc[0]) * (loc[3] - loc[1]))
 
-    # Calculer les fichiers de reconnaissance faciale pour chaque image
-    face_encodings = []
-    for image_path in image_paths:
-        image = face_recognition.load_image_file(image_path)
-        face_encoding = face_recognition.face_encodings(image)[0]
-        face_encodings.append(face_encoding)
+    # Rechercher une correspondance de visage parmi les fichiers existants
+    persons_path = Path("persons")
+    for person_folder in persons_path.iterdir():
+        face_encodings = []
+        for face_encoding_path in person_folder.glob("*.npy"):
+            face_encodings.append(np.load(str(face_encoding_path)))
 
-    # Sauvegarder les fichiers de reconnaissance faciale dans le dossier de la personne
-    for i, face_encoding in enumerate(face_encodings):
-        face_encoding_path = person_path / f"face_{i}.npy"
-        np.save(str(face_encoding_path), face_encoding)
+        face_encodings = np.array(face_encodings)  # Convertir la liste en tableau NumPy
 
-    return {"message": "Person added successfully"}
+        face_distances = face_recognition.face_distance(face_encodings, face_encodings)
+        best_match_index = np.argmin(face_distances)
+
+        if face_distances[best_match_index] < 0.6:
+            # Correspondance trouvée, renvoyer le fichier de données JSON de la personne
+            data_path = person_folder / "data.json"
+            with open(data_path) as f:
+                data = json.load(f)
+            return {"person": data}
+
+    return {"message": "No match found"}
 
 
 @app.post("/search")
