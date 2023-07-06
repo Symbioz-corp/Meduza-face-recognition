325f7a77d2fb2d6e3c6a28305f9a644d601d119c
Rakotonimanana T. Andritiana Steve <pro@andritiana.tech>
2023-07-06 12:09:31 +0300

    fix

--- main.py
+++ main.py
@@ -15,44 +15,6 @@ app = FastAPI()
 async def search_person(image: UploadFile = File(...)):
     # Charger l'image et détecter les visages
     image_data = image.file.read()
-    image = Image.open(io.BytesIO(image_data))
-    image = np.array(image)  # Convertir l'image en tableau NumPy
-    face_locations = face_recognition.face_locations(image)
-
-    if len(face_locations) == 0:
-        return {"message": "No face detected"}
-
-    # Trouver le visage le plus grand
-    largest_face_location = max(face_locations, key=lambda loc: (loc[2] - loc[0]) * (loc[3] - loc[1]))
-
-    # Rechercher une correspondance de visage parmi les fichiers existants
-    persons_path = Path("persons")
-    for person_folder in persons_path.iterdir():
-        face_encodings = []
-        for face_encoding_path in person_folder.glob("*.npy"):
-            face_encodings.append(np.load(str(face_encoding_path)))
-
-        face_encodings = np.array(face_encodings)  # Convertir la liste en tableau NumPy
-
-        face_distances = face_recognition.face_distance(face_encodings, face_encodings)
-        best_match_index = np.argmin(face_distances)
-
-        if face_distances[best_match_index] < 0.6:
-            # Correspondance trouvée, renvoyer le fichier de données JSON de la personne
-            data_path = person_folder / "data.json"
-            with open(data_path) as f:
-                data = json.load(f)
-            return {"person": data}
-
-    return {"message": "No match found"}
-
-
-import numpy as np
-
-@app.post("/search")
-async def search_person(image: UploadFile = File(...)):
-    # Charger l'image et détecter les visages
-    image_data = image.file.read()
     image = face_recognition.load_image_file(image_data)
     face_locations = face_recognition.face_locations(image)
 
@@ -62,8 +24,8 @@ async def search_person(image: UploadFile = File(...)):
     # Trouver le visage le plus grand
     largest_face_location = max(face_locations, key=lambda loc: (loc[2] - loc[0]) * (loc[3] - loc[1]))
 
-    # Calculer les encodages de visage de l'image de recherche
-    image_encodings = face_recognition.face_encodings(image, [largest_face_location])
+    # Calculer l'encodage de visage de l'image de recherche
+    image_encoding = face_recognition.face_encodings(image, [largest_face_location])[0]
 
     # Rechercher une correspondance de visage parmi les fichiers existants
     persons_path = Path("persons")
@@ -75,7 +37,7 @@ async def search_person(image: UploadFile = File(...)):
         for face_encoding_path in person_folder.glob("*.npy"):
             face_encodings.append(np.load(str(face_encoding_path)))
 
-        face_distances = face_recognition.face_distance(face_encodings, image_encodings[0])
+        face_distances = face_recognition.face_distance(face_encodings, image_encoding)
         best_match_index = np.argmin(face_distances)
 
         if face_distances[best_match_index] < best_match_distance:
@@ -91,7 +53,6 @@ async def search_person(image: UploadFile = File(...)):
         return {"person": best_match_data}
 
     return {"message": "No match found"}
-
 
 # Endpoint pour ajouter une personne
 @app.post("/add")
